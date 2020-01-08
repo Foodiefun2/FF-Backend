4d5fc51573489060244a9f1f5ef0f89b0207d253
Raymond Trinh <RaymondTrinh91@gmail.com>
2020-01-08 09:19:06 -0800

    fixed a few more typos

--- api/server.js
+++ api/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const configMiddleware = require("../middleware.js");
 
 const authRouter = require("./auth/auth-router.js");
+const userRouter = require("./users/users-router.js");
+const restRouter = require("./restaurant/restaurant-router.js");
 
 const server = express();
 
 configMiddleware(server);
 
 server.use("/api/auth", authRouter);
+server.use("/api/users", userRouter);
+server.use("/api/restaurants", restRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });

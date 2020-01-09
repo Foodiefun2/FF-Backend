eae0330faa57d0d03c551bf80c117c0614c07080
Raymond Trinh <RaymondTrinh91@gmail.com>
2020-01-09 03:06:14 -0800

    changed truncate back to del

--- data/seeds/004-ratings.js
+++ data/seeds/004-ratings.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('rest_ratings').truncate()
+  return knex('rest_ratings').del()
     .then(function () {
       // Inserts seed entries
       return knex('rest_ratings').insert([
